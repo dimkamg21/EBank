ddd4ccc2180d569e0be54b47208886f8170b1191
Orest Kozak <orestkozak17@gmail.com>
2023-10-18 01:23:03 +0300

    update type-checkbox

--- src/components/PersonalAccount/TransactionPage/Transaction.tsx
+++ src/components/PersonalAccount/TransactionPage/Transaction.tsx
@@ -1,8 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { CardContext } from '../CardContext/CardContext';
-// import { Card } from '../../../types/Card';
 import { CardTemplate } from '../CardsPage/CardTemplate/CardTempate';
-import '../CardsPage/CardList/CardList.scss';
 import './Transaction.scss';
 import { Transaction, TransactionType } from '../../../types/Transactions';
 
@@ -12,54 +10,68 @@ export const TransactionPage: React.FC = () => {
   const cardContext = useContext(CardContext);
   const { userCards, setUserCards } = cardContext;
 
-
   const [currentCard, setCurrentCard] = useState(0);
   const [showCVV, setShowCVV] = useState(false);
 
+  const [withdrawalData, setWithdrawalData] = useState({
+    recipientCard: '',
+    amount: '',
+    paymentPurpose: '',
+  });
+
+  const [mobileTopUpData, setMobileTopUpData] = useState({
+    phoneNumber: '',
+    amount: '',
+  });
+
   const handleMakePayment = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
-    // Отримайте дані з форми
-    const recipientCardElement = e.currentTarget.querySelector<HTMLInputElement>('[name="cardNumber"]');
-    const amountElement = e.currentTarget.querySelector<HTMLInputElement>('[placeholder="Amount"]');
-    const paymentPurposeElement = e.currentTarget.querySelector<HTMLInputElement>('[placeholder="Payment Purpose"]');
-    
-    if (recipientCardElement && amountElement && paymentPurposeElement) {
-        const recipientCard = recipientCardElement.value;
-        const amount = amountElement.value;
-        const paymentPurpose = paymentPurposeElement.value;
-        
-        // Знайдіть поточну карту, з якої надсилається переказ
-        const senderCard = userCards[currentCard];
-      
-        // Оновіть баланс кожної карти
-        const updatedUserCards = userCards.map((card, index) => {
-          if (index === currentCard) {
-            const newBalance = card.balance - parseFloat(amount);
-            return { ...card, balance: newBalance };
-          }
-    
-          return card;
-        });
-      
-        // Оновіть стан користувача та історію переказів
-        setUserCards(updatedUserCards);
-        setTransactionHistory([
-          {
-            senderCard: senderCard.cardNumber,
-            recipientCard,
-            type: 'withdrawal',
-            amount: parseFloat(amount),
-            balance: senderCard.balance - parseFloat(amount),
-            paymentPurpose,
-          },
-          ...transactionHistory,
-        ]);
-        
-        // Очистіть поля вводу
-        e.currentTarget.reset();
+
+    if (transactionType === 'withdrawal') {
+      const { recipientCard, amount, paymentPurpose } = withdrawalData;
+
+      const senderCard = userCards[currentCard];
+
+      const updatedUserCards = userCards.map((card, index) => {
+        if (index === currentCard) {
+          const newBalance = card.balance - parseFloat(amount);
+          return { ...card, balance: newBalance };
+        }
+        return card;
+      });
+
+      setUserCards(updatedUserCards);
+      setTransactionHistory([
+        {
+          senderCard: senderCard.cardNumber,
+          recipientCard,
+          type: 'withdrawal',
+          amount: parseFloat(amount),
+          balance: senderCard.balance - parseFloat(amount),
+          paymentPurpose,
+        },
+        ...transactionHistory,
+      ]);
+
+      e.currentTarget.reset();
+    } else if (transactionType === 'mobile-top-up') {
+      const { phoneNumber, amount } = mobileTopUpData;
+
+      setUserCards(userCards);
+      setTransactionHistory([
+        {
+          senderCard: userCards[currentCard].cardNumber,
+          phoneNumber: phoneNumber,
+          type: 'mobile-top-up',
+          amount: parseFloat(amount),
+          balance: userCards[currentCard].balance - parseFloat(amount),
+        },
+        ...transactionHistory,
+      ]);
+
+      e.currentTarget.reset();
     }
-};
+  };
 
   const nextSlide = () => {
     setCurrentCard((prevSlide) => (prevSlide === userCards.length - 1 ? 0 : prevSlide + 1));
@@ -70,10 +82,8 @@ export const TransactionPage: React.FC = () => {
   };
 
   useEffect(() => {
-
     const cardElement = document.getElementById('cards-trans');
     if (cardElement) {
-
       cardElement.classList.add('fade-out');
       setTimeout(() => {
         cardElement.classList.remove('fade-out');
@@ -85,26 +95,18 @@ export const TransactionPage: React.FC = () => {
     <div className="container-page">
       <div className="container-page__top">
         <div className="container-l">
-          <button className="prev-button" onClick={prevSlide}>{'<'}</button>
-
-          <div
-            id="cards-trans"
-            onDoubleClick={() => setShowCVV(!showCVV)}
-            className="cards-container"
-          >
+          <button className="prev-button" onClick={prevSlide}>
+            {'<'}
+          </button>
+          <div id="cards-trans" onDoubleClick={() => setShowCVV(!showCVV)} className="cards-container">
             {userCards && <CardTemplate isHovered={showCVV} cardData={userCards[currentCard]} />}
-
           </div>
-
-          <button className="next-button" onClick={nextSlide}>{'>'}</button>
+          <button className="next-button" onClick={nextSlide}>
+            {'>'}
+          </button>
         </div>
-
         {transactionType === 'withdrawal' && (
-          <form
-            action=""
-            className="input-container"
-            onSubmit={handleMakePayment}
-          >
+          <form action="" className="input-container" onSubmit={handleMakePayment}>
             <input
               type="text"
               placeholder="Recipient's card"
@@ -112,76 +114,116 @@ export const TransactionPage: React.FC = () => {
               maxLength={16}
               className="card-number-input"
               minLength={16}
+              onChange={(e) =>
+                setWithdrawalData({
+                  ...withdrawalData,
+                  recipientCard: e.target.value,
+                })
+              }
             />
             <input
               type="text"
               placeholder="Amount"
               className="card-number-input"
+              onChange={(e) =>
+                setWithdrawalData({
+                  ...withdrawalData,
+                  amount: e.target.value,
+                })
+              }
             />
             <input
               type="text"
               placeholder="Payment Purpose"
               name="paymentPurpose"
               className="card-number-input"
+              onChange={(e) =>
+                setWithdrawalData({
+                  ...withdrawalData,
+                  paymentPurpose: e.target.value,
+                })
+              }
             />
-
             <button type="submit">Make payment</button>
           </form>
         )}
 
-{transactionType === 'mobile-top-up' && (
+        {transactionType === 'mobile-top-up' && (
           <form action="" className="input-container" onSubmit={handleMakePayment}>
             <input
               type="text"
               placeholder="Recipient's phone number"
               name="phoneNumber"
-              maxLength={10} // Максимальна довжина номера телефону
+              maxLength={10}
               className="card-number-input"
-              minLength={10} // Мінімальна довжина номера телефону
+              minLength={10}
+              onChange={(e) =>
+                setMobileTopUpData({
+                  ...mobileTopUpData,
+                  phoneNumber: e.target.value,
+                })
+              }
             />
-            <input type="text" placeholder="Amount" className="card-number-input" />
-            <button type="submit">Make payment</button>
+            <input
+              type="text"
+              placeholder="Amount"
+              className="card-number-input"
+              onChange={(e) =>
+                setMobileTopUpData({
+                  ...mobileTopUpData,
+                  amount: e.target.value,
+                })
+              }
+            />
+            <button type="submit">Top-Up</button>
           </form>
         )}
       </div>
-
-      <div className="transaction-type-toggle">
-        <label>
-          <input
-            type="radio"
-            value="withdrawal"
-            checked={transactionType === 'withdrawal'}
-            onChange={() => setTransactionType('withdrawal')}
-          />
-          Withdrawal
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="mobile-top-up"
-            checked={transactionType === 'mobile-top-up'}
-            onChange={() => setTransactionType('mobile-top-up')}
-          />
-          Mobile Top-Up
-        </label>
-      </div>
-
-      {/* {transactionHistory.length > 0 && (
+        <div className="transaction-type-toggle">
+          <label>
+            <input
+              type="radio"
+              value="withdrawal"
+              checked={transactionType === 'withdrawal'}
+              onChange={() => setTransactionType('withdrawal')}
+            />
+            Withdrawal
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="mobile-top-up"
+              checked={transactionType === 'mobile-top-up'}
+              onChange={() => setTransactionType('mobile-top-up')}
+            />
+            Mobile Top-Up
+          </label>
+        </div>
+      {transactionHistory.length > 0 && (
         <div className="transaction-history">
           <h2>Transaction History</h2>
           <ul>
             {transactionHistory.map((transaction, index) => (
               <li key={index} className="transaction-item">
-                {` Sender Card: ${transaction.senderCard}   
-                Recipient Card: ${transaction.recipientCard}   
-                Balance: ${transaction.balance}    
+                {` Sender Card: ${transaction.senderCard}
+                ${
+                  transaction.type === 'withdrawal'
+                    ? `Recipient Card: ${transaction.recipientCard}`
+                    : `Recipient Phone Number: ${transaction.phoneNumber}`
+                }
+                Balance: ${transaction.balance}
                 Amount: ${transaction.amount}
-                Payment Purpose: ${transaction.paymentPurpose}`}
+                ${
+                  transaction.type === 'withdrawal'
+                    ? `Payment Purpose: ${transaction.paymentPurpose}
+                       Type: Transfer of funds`
+                    : 'Type: Mobile top-up'
+                }`}
               </li>
             ))}
           </ul>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
